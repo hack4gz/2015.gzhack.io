c78faa4c581321b8b2ead2320bddaa838b95ea77
Martin <hongzeqin@gmail.com>
2015-06-06 02:20:26 +0800

    works for working with activeadmin and works

---
+++ app/admin/work.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Work do
+  index do
+    selectable_column
+    column :id
+    column :team
+    column :title
+    column :introduction
+    column "文档附件" do |work|
+      links = work.attachments.map do |attachment|
+        link_to attachment.file.try(:filename), attachment.url, target: '_blank'
+      end
+
+      raw links.join('<br><br>')
+    end
+    column :created_at
+
+    actions
+  end
+end
