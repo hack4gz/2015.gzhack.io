d2a17f86957284ccefc30fcc9dba839c6a91be68
Martin <hongzeqin@gmail.com>
2015-06-05 23:05:55 +0800

    add works table and run database migration

---
+++ db/migrate/20150605150254_create_works.rb
@@ -0,0 +1,11 @@
+class CreateWorks < ActiveRecord::Migration
+  def change
+    create_table :works do |t|
+      t.string :team
+      t.string :title
+      t.text :introduction
+
+      t.timestamps null: false
+    end
+  end
+end
